fb0a9eb5776267ebfb833c66bac5fcee83a278aa
Anastasiia Yerashova <yerashova.a@gmail.com>
2025-02-24 15:31:22 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import { initMongoConnection } from "./db/initMongoConnection.js"
 import { startServer } from "./server.js"
 
 (async () => {
+    await initMongoConnection()
     startServer()
 })()
